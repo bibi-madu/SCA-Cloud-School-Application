a580f70f655fc89b1331dc0871c11994ac13d3b6
bibi-madu <87828758+bibi-madu@users.noreply.github.com>
2021-07-23 14:17:35 +0100

    Create helloadd.py

---
+++ helloadd.py
@@ -0,0 +1,10 @@
+# This program adds two numbers 
+
+num1 = 678
+num2 = 789
+
+# Add both numbers
+sum = num1 + num2 
+
+#Display total
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
